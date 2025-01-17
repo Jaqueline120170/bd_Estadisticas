e58020639a2034f6af66e62539a5985180e7a600
DESKTOP-I9JNUFA\PC <csi23-jromres@colegioaltair.com>
2025-01-17 13:38:38 +0100

    no message

--- src/main/java/com/estadisticas/estadisticas_app/Modelos/Dataset.java
+++ src/main/java/com/estadisticas/estadisticas_app/Modelos/Dataset.java
@@ -1,5 +1,28 @@
 package com.estadisticas.estadisticas_app.Modelos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "datasets", schema = "gestion")
 public class Dataset {
 
+		@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "id_dataset")
+	    private Long id;
+		
+		@Column(name = "nombre_dataset")
+	    private String nombreDataset;
+		
+		@Column(name = "fuente_dataset")
+	    private String fuenteDataset;
+		@Column(name = "nombre_usuario")
+	    private String descripcionDataset;
+	    private String archivoUrl;
+	    private String fechaActualizacionDataset;
 }
