5bc9f3902417eb75511626cc1457a76d71f4c7c6
Jaqueline Romero <jaqelineromto@gmail.com>
2025-05-24 21:45:03 +0200

    Agregando configuración CORS para producción

--- src/main/java/com/estadisticas/estadisticas_app/EstadisticasAppApplication.java
+++ src/main/java/com/estadisticas/estadisticas_app/EstadisticasAppApplication.java
@@ -1,5 +1,7 @@
 package com.estadisticas.estadisticas_app;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -17,8 +19,13 @@ public class EstadisticasAppApplication {
      * 
      * @param args Argumentos de línea de comandos
      */
-    public static void main(String[] args) {
-        // Arranca la aplicación Spring Boot
-        SpringApplication.run(EstadisticasAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+	    SpringApplication app = new SpringApplication(EstadisticasAppApplication.class);
+	    String port = System.getenv("PORT");
+	    if (port != null) {
+	        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+	    }
+	    app.run(args);
+	}
+
 }
