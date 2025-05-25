a3479df975fd9372cf27ba37488b428d3a907404
Jaqueline Romero <jaqelineromto@gmail.com>
2025-05-25 16:06:31 +0200

    test2

--- src/main/java/com/estadisticas/estadisticas_app/Controladores/TestControlador.java
+++ src/main/java/com/estadisticas/estadisticas_app/Controladores/TestControlador.java
@@ -1,17 +1,25 @@
 package com.estadisticas.estadisticas_app.Controladores;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestControlador {
-	
+	private static final Logger logger = LoggerFactory.getLogger(TestControlador.class);
 	  @GetMapping
 	    public ResponseEntity<String> testApi() {
 	        return ResponseEntity.ok("API FUNCIONANDO");
 	    }
-	
+	  @PostConstruct
+	  public void init() {
+	      logger.info("Aplicación corriendo en puerto: {}", System.getenv("PORT"));
+	  }
+
 }
